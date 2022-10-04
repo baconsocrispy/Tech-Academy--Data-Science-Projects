f6c1844bb0a6a6f19bcc8515c47de4f22fdc2200
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2022-10-03 17:25:38 -0700

    Added chi-square test

---
+++ formulas.py
@@ -0,0 +1,12 @@
+# GOODNESS OF FIT TESTS
+
+# Chi-Square Test
+# symbol: x2
+# O = Observed Statistic
+# E = Expected Statistic (will be constant)
+
+# value equals the sum of the differences squared of all events
+# divided by the expected statistc
+# x2 = Sum (O - E)^2 / E
+
+# 
